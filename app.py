de120daf835b8fa89d1d3278ea48cf316d31ff00
Sudhir <spelsudhir@gmail.com>
2026-09-13 01:05:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,15 @@ from scipy.stats import norm
 
 warnings.filterwarnings("ignore")
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 # ======================================================================
 # 1. PAGE CONFIG & GLOBAL STYLING
 # ======================================================================
